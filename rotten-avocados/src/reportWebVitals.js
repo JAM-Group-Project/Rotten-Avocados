199305be576077e9d235545749ef3a2ec71fa8a8
Alex-Ng10 <nguye5aq@dukes.jmu.edu>
2025-06-18 10:48:05 -0400

    test

--- rotten-avocados/src/reportWebVitals.js
+++ rotten-avocados/src/reportWebVitals.js
@@ -1,3 +1,7 @@
+// src/reportWebVitals.js
+// This file is used to measure and report web vitals in a Create React App project.
+// You can customize or remove this file if you don't need web vitals reporting.
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
