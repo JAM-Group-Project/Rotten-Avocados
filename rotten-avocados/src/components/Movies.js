744040f5de7ffb2d6e5d925208bc77eaf58e4af8
Alex Nguyen <nguye5aq@dukes.jmu.edu>
2025-06-30 23:06:36 -0400

    api added

--- rotten-avocados/src/components/Movies.js
+++ rotten-avocados/src/components/Movies.js
@@ -5,11 +5,35 @@ function Movie({ searchTerm }) {
   const API_KEY = "6bff30742e46b5d624e5b0376351ba35";
   const [movieList, setMovieList] = useState([]);
 
-  const getMovie = () => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+  // const getMovie = () => {
+  //   fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+  //     .then(res => res.json())
+  //     .then(json => setMovieList(json.results));
+  // };
+
+    const TrendingMovies = () => {
+    fetch (`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`)
       .then(res => res.json())
-      .then(json => setMovieList(json.results));
+      .then(json => setMovieList(json.results || []));
+  }
+
+  const searchMovies = (query) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`)
+      .then(res => res.json())
+      .then(json => setMovieList(json.results || []));
   };
+
+  // API for shows trending:
+  // curl --request GET \
+  //    --url 'https://api.themoviedb.org/3/trending/tv/day?language=en-US' \
+  //    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YmZmMzA3NDJlNDZiNWQ2MjRlNWIwMzc2MzUxYmEzNSIsIm5iZiI6MTc1MDI1NjYxMS4xMjIsInN1YiI6IjY4NTJjYmUzZjZiYzkxNGJiNWZiNTJiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GJCLdC161u5UYFrlZaE8Vk2w8aTmWBaiKoChuCEgjvw' \
+  //    --header 'accept: application/json'
+
+  // API for shows search: 
+  // curl --request GET \
+  //    --url 'https://api.themoviedb.org/3/search/tv?include_adult=false&language=en-US&page=1' \
+  //    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YmZmMzA3NDJlNDZiNWQ2MjRlNWIwMzc2MzUxYmEzNSIsIm5iZiI6MTc1MDI1NjYxMS4xMjIsInN1YiI6IjY4NTJjYmUzZjZiYzkxNGJiNWZiNTJiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GJCLdC161u5UYFrlZaE8Vk2w8aTmWBaiKoChuCEgjvw' \
+  //    --header 'accept: application/json'
 
   useEffect(() => {
     getMovie();
