b403761b6962ec462c5f04d1fa3feba2e9918439
Alex-Ng10 <nguye5aq@dukes.jmu.edu>
2025-07-09 11:05:16 -0400

    Fixed bugs

--- rotten-avocados/src/index.js
+++ rotten-avocados/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/Home';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
